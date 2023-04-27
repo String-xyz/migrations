e9f05a7eda7abba23d47fc7e5e713bdedfd2daad
Ocasta <ocasta181@gmail.com>
2023-04-27 14:05:09 -0700

    add deleted_at; fix data seeding

--- migrations/00002_data_initialization.go
+++ migrations/00002_data_initialization.go
@@ -2,8 +2,10 @@ package migrations
 
 import (
 	"database/sql"
+	"fmt"
 	"os"
 
+	"github.com/google/uuid"
 	"github.com/pressly/goose/v3"
 )
 
@@ -13,30 +15,48 @@ func init() {
 
 func Up00002(tx *sql.Tx) error {
 	// Insert networks
-	_, err := tx.Exec(`
+	query := `
 		INSERT INTO network (name, network_id, chain_id, gas_oracle, rpc_url, explorer_url) VALUES
-		('Polygon Mainnet', 137, 137, 'poly', 'https://rpc-mainnet.matic.quiknode.pro', 'https://polygonscan.com'),
-		('Mumbai Testnet', 80001, 80001, 'poly', 'https://matic-mumbai.chainstacklabs.com', 'https://mumbai.polygonscan.com'),
-		('Goerli Testnet', 5, 5, 'eth', 'https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161', 'https://goerli.etherscan.io'),
-		('Ethereum Mainnet', 1, 1, 'eth', 'https://rpc.ankr.com/eth', 'https://etherscan.io'),
-		('Fuji Testnet', 1, 43113, 'avax', 'https://api.avax-test.network/ext/bc/C/rpc', 'https://testnet.snowtrace.io'),
-		('Avalanche Mainnet', 1, 43114, 'avax', 'https://api.avax.network/ext/bc/C/rpc', 'https://snowtrace.io'),
-		('Nitro Goerli Rollup Testnet', 421613, 421613, 'arb', 'https://goerli-rollup.arbitrum.io/rpc', 'https://goerli.arbiscan.io'),
-		('Arbitrum Nova Mainnet', 42170, 42170, 'arb', 'https://nova.arbitrum.io/rpc', 'https://nova-explorer.arbitrum.io');
-	`)
+			('Polygon Mainnet', 137, 137, 'poly', 'https://rpc-mainnet.matic.quiknode.pro', 'https://polygonscan.com'),
+			('Mumbai Testnet', 80001, 80001, 'poly', 'https://matic-mumbai.chainstacklabs.com', 'https://mumbai.polygonscan.com'),
+			('Goerli Testnet', 5, 5, 'eth', 'https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161', 'https://goerli.etherscan.io'),
+			('Ethereum Mainnet', 1, 1, 'eth', 'https://rpc.ankr.com/eth', 'https://etherscan.io'),
+			('Fuji Testnet', 1, 43113, 'avax', 'https://api.avax-test.network/ext/bc/C/rpc', 'https://testnet.snowtrace.io'),
+			('Avalanche Mainnet', 1, 43114, 'avax', 'https://api.avax.network/ext/bc/C/rpc', 'https://snowtrace.io'),
+			('Nitro Goerli Rollup Testnet', 421613, 421613, 'arb', 'https://goerli-rollup.arbitrum.io/rpc', 'https://goerli.arbiscan.io'),
+			('Arbitrum Nova Mainnet', 42170, 42170, 'arb', 'https://nova.arbitrum.io/rpc', 'https://nova-explorer.arbitrum.io')
+		RETURNING id;
+		`
+
+	rows, err := tx.Query(query)
 	if err != nil {
 		return err
 	}
+	defer rows.Close()
 
-	// Insert assets
-	_, err = tx.Exec(`
+	var ids []uuid.UUID
+	for rows.Next() {
+		var id uuid.UUID
+		err := rows.Scan(&id)
+		if err != nil {
+			return err
+		}
+		ids = append(ids, id)
+	}
+	if err := rows.Err(); err != nil {
+		return err
+	}
+
+	query2 := fmt.Sprintf(`
 		INSERT INTO asset (name, description, decimals, is_crypto, network_id, value_oracle, value_oracle_2) VALUES
-		('AVAX', 'Avalanche', 18, true, 6, 'avalanche-2', 'avalanche'),
-		('ETH', 'Ethereum', 18, true, 4, 'ethereum', 'ethereum'),
-		('MATIC', 'Matic', 18, true, 1, 'matic-network', 'matic'),
-		('GOERLIETH', 'Goerli Ethereum', 18, true, 7, 'ethereum', 'ethereum'),
+		('AVAX', 'Avalanche', 18, true, '%s', 'avalanche-2', 'avalanche'),
+		('ETH', 'Ethereum', 18, true, '%s', 'ethereum', 'ethereum'),
+		('MATIC', 'Matic', 18, true, '%s', 'matic-network', 'matic'),
+		('GOERLIETH', 'Goerli Ethereum', 18, true, '%s', 'ethereum', 'ethereum'),
 		('USD', 'United States Dollar', 6, false, NULL, NULL, NULL);
-	`)
+	`, ids[0], ids[1], ids[2], ids[3])
+
+	_, err = tx.Exec(query2)
 	if err != nil {
 		return err
 	}
@@ -44,7 +64,7 @@ func Up00002(tx *sql.Tx) error {
 	// Update networks with gas_token_id
 	updateStmt := `
 		UPDATE network
-		SET gas_token_id = (SELECT id FROM assets WHERE name = $1)
+		SET gas_token_id = (SELECT id FROM asset WHERE name = $1)
 		WHERE name = $2;
 	`
 	updates := [8][2]string{
@@ -67,7 +87,7 @@ func Up00002(tx *sql.Tx) error {
 
 	// Insert String User
 	var stringUserId string
-	row := tx.QueryRow(`INSERT INTO string-user (type, status) VALUES ('internal', 'internal') RETURNING id;`)
+	row := tx.QueryRow(`INSERT INTO string_user (type, status) VALUES ('internal', 'internal') RETURNING id;`)
 	if err := row.Scan(&stringUserId); err != nil {
 		return err
 	}
@@ -78,7 +98,7 @@ func Up00002(tx *sql.Tx) error {
 		panic("STRING_INTERNAL_ID is not set in ENV!")
 	}
 
-	_, err = tx.Exec(`UPDATE users SET id = $1 WHERE id = $2;`, internalId, stringUserId)
+	_, err = tx.Exec(`UPDATE string_user SET id = $1 WHERE id = $2;`, internalId, stringUserId)
 	if err != nil {
 		return err
 	}
