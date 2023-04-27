6ae90082ec09584ca1be31eee736fdb433999ce5
Ocasta <ocasta181@gmail.com>
2023-04-27 14:36:06 -0700

    remove cruft from entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,14 +7,8 @@ echo $(ls -a)
 
 # run db migrations
 echo "----- Building migrations..."
-# can't build from inside the container yet
 go build -o migrations/goose-migrate cmd/main.go
 echo "----- Running migrations..."
-# cd migrations
 ./migrations/goose-migrate reset
 ./migrations/goose-migrate up
-# DB_CONFIG="host=$DB_HOST user=$DB_USERNAME dbname=$DB_NAME sslmode=disable password=$DB_PASSWORD"
-# goose postgres "$DB_CONFIG" reset
-# goose postgres "$DB_CONFIG" up
-# cd ..
 echo "----- ...Migrations done"
