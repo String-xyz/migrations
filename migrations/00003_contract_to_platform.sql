f3292589831fca43b38b4e999dcc49e865ab8003
____ <saito.sv.h@gmail.com>
2023-07-09 16:20:49 -0700

    create a new migration file since we are on prod now

---
+++ migrations/00003_contract_to_platform.sql
@@ -0,0 +1,31 @@
+-------------------------------------------------------------------------
+-- CONTRACT_TO_PLATFORM -------------------------------------------------
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE contract_to_platform (
+	contract_id UUID REFERENCES contract (id),
+	platform_id UUID REFERENCES platform (id)
+);
+
+CREATE UNIQUE INDEX contract_to_platform_contract_id_platform_id_idx ON contract_to_platform(contract_id, platform_id);
+-------------------------------------------------------------------------
+-- ALTER CONTRACT TABLE -------------------------------------------------
+ALTER TABLE contract DROP COLUMN platform_id;
+ALTER TABLE ADD COLUMN organization_id UUID REFERENCES organization (id);
+-- Lets add a unique constraint so we can't have duplicates in the same network and organization
+CREATE UNIQUE INDEX contract_address_network_id_org_id_idx ON contract(address,organization_id,network_id);
+-- +goose StatementEnd
+
+-------------------------------------------------------------------------
+--         ______                         ____                    
+--        / ____/___  ____  ________     / __ \____ _      ______ 
+--       / / __/ __ \/ __ \/ ___/ _ \   / / / / __ \ | /| / / __ \
+--      / /_/ / /_/ / /_/ (__  )  __/  / /_/ / /_/ / |/ |/ / / / /
+--      \____/\____/\____/____/\___/  /_____/\____/|__/|__/_/ /_/ 
+-------------------------------------------------------------------------  
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS contract_to_platform;
+ALTER TABLE contract DROP COLUMN organization_id;
+ALTER TABLE contract ADD COLUMN platform_id UUID REFERENCES platform (id);
+-- +goose StatementEnd
