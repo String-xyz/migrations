c7081dc62c8e6f2523a205051ecf7d5309addc4f
Ocasta <ocasta181@gmail.com>
2023-07-10 00:24:38 -0400

    small syntax fixes

--- migrations/00003_contract_to_platform.sql
+++ migrations/00003_contract_to_platform.sql
@@ -10,8 +10,11 @@ CREATE TABLE contract_to_platform (
 CREATE UNIQUE INDEX contract_to_platform_contract_id_platform_id_idx ON contract_to_platform(contract_id, platform_id);
 -------------------------------------------------------------------------
 -- ALTER CONTRACT TABLE -------------------------------------------------
-ALTER TABLE contract DROP COLUMN platform_id;
-ALTER TABLE ADD COLUMN organization_id UUID REFERENCES organization (id);
+ALTER TABLE contract 
+	DROP COLUMN IF EXISTS platform_id,
+	ADD COLUMN organization_id UUID REFERENCES organization (id);
+
+
 -- Lets add a unique constraint so we can't have duplicates in the same network and organization
 CREATE UNIQUE INDEX contract_address_network_id_org_id_idx ON contract(address,organization_id,network_id);
 -- +goose StatementEnd
@@ -27,6 +30,7 @@ CREATE UNIQUE INDEX contract_address_network_id_org_id_idx ON contract(address,o
 -- +goose StatementBegin
 DROP TABLE IF EXISTS contract_to_platform;
 DROP INDEX IF EXISTS contract_address_network_id_org_id_idx;
-ALTER TABLE contract DROP COLUMN organization_id;
-ALTER TABLE contract ADD COLUMN platform_id UUID REFERENCES platform (id);
+ALTER TABLE contract 
+	DROP COLUMN organization_id,
+	ADD COLUMN platform_id UUID REFERENCES platform (id);
 -- +goose StatementEnd
