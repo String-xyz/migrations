9cd6ddbcdf90688c851020ea33f8ada5ac49500c
saito-sv <7920256+saito-sv@users.noreply.github.com>
2023-07-09 21:28:23 -0700

    Contract Creation with multiple Platform IDs (STR-654) (#7)
    * Add contracts-to-platform table
    
    * Add org_id to Contract
    
    * added constraint
    
    * create a new migration file since we are on prod now
    
    * create a new migration file since we are on prod now
    
    * create a new migration file since we are on prod now
    
    * tab spaces to 4
    
    * Update migrations/00001_schema_initialization.sql
    
    * drop constraint
    
    * Update migrations/00003_contract_to_platform.sql
    
    * small syntax fixes
    
    ---------
    
    Co-authored-by: frostbournesb <85953565+frostbournesb@users.noreply.github.com>
    Co-authored-by: Ocasta <ocasta181@gmail.com>

---
+++ migrations/00003_contract_to_platform.sql
@@ -0,0 +1,36 @@
+-------------------------------------------------------------------------
+-- CONTRACT_TO_PLATFORM -------------------------------------------------
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE contract_to_platform (
+	contract_id UUID REFERENCES contract (id),
+	platform_id UUID REFERENCES platform (id)
+);
+
+CREATE UNIQUE INDEX contract_to_platform_contract_id_platform_id_idx ON contract_to_platform(contract_id, platform_id);
+-------------------------------------------------------------------------
+-- ALTER CONTRACT TABLE -------------------------------------------------
+ALTER TABLE contract 
+	DROP COLUMN IF EXISTS platform_id,
+	ADD COLUMN organization_id UUID REFERENCES organization (id);
+
+
+-- Lets add a unique constraint so we can't have duplicates in the same network and organization
+CREATE UNIQUE INDEX contract_address_network_id_org_id_idx ON contract(address,organization_id,network_id);
+-- +goose StatementEnd
+
+-------------------------------------------------------------------------
+--         ______                         ____                    
+--        / ____/___  ____  ________     / __ \____ _      ______ 
+--       / / __/ __ \/ __ \/ ___/ _ \   / / / / __ \ | /| / / __ \
+--      / /_/ / /_/ / /_/ (__  )  __/  / /_/ / /_/ / |/ |/ / / / /
+--      \____/\____/\____/____/\___/  /_____/\____/|__/|__/_/ /_/ 
+-------------------------------------------------------------------------  
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS contract_to_platform;
+DROP INDEX IF EXISTS contract_address_network_id_org_id_idx;
+ALTER TABLE contract 
+	DROP COLUMN organization_id,
+	ADD COLUMN platform_id UUID REFERENCES platform (id);
+-- +goose StatementEnd
