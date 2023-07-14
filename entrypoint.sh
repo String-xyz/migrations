4c3759bccc218dad025564d8b549d78c38bf069c
Ocasta <ocasta181@gmail.com>
2023-07-14 19:03:14 -0400

    schema migraiton #5

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@
 echo "----- Building migrations..."
 go build -o migrations/goose-migrate cmd/main.go
 echo "----- Running migrations..."
-./migrations/goose-migrate reset
+# ./migrations/goose-migrate reset
 ./migrations/goose-migrate up
 echo "----- ...Migrations done"
