8052a37109a8f6b4036aa05e0392d7a7d74c7f8f
akfoster <ocasta181@gmail.com>
2023-07-27 18:56:21 -0400

    Create Identity Table (#18)
    * add identity to migration 5
    
    * ensure table exists before dropping
    
    * change level to a number
    
    * Update migrations/00005_schema_asset_contract_identity.sql
    
    Co-authored-by: Frostbourne <85953565+frostbournesb@users.noreply.github.com>
    
    * Update migrations/00005_schema_asset_contract_identity.sql
    
    ---------
    
    Co-authored-by: Frostbourne <85953565+frostbournesb@users.noreply.github.com>

--- migrations/00005_schema_asset_contract_identity.sql
+++ migrations/00005_schema_asset_contract_identity.sql
@@ -1,6 +1,28 @@
 -- +goose Up
 
 -------------------------------------------------------------------------
+-- IDENTITY -------------------------------------------------------------
+CREATE TABLE identity (
+    id UUID PRIMARY KEY NOT NULL DEFAULT UUID_GENERATE_V4(),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP WITH TIME ZONE DEFAULT NULL,
+    level INT DEFAULT 0,
+    account_id TEXT DEFAULT '' NOT NULL,
+    user_id TEXT NOT NULL,
+    email_verified TIMESTAMP WITH TIME ZONE DEFAULT NULL,
+    phone_verified TIMESTAMP WITH TIME ZONE DEFAULT NULL,
+    selfie_verified TIMESTAMP WITH TIME ZONE DEFAULT NULL,
+    document_verified TIMESTAMP WITH TIME ZONE DEFAULT NULL
+);
+
+CREATE OR REPLACE TRIGGER update_identity_updated_at 
+    BEFORE UPDATE 
+    ON identity 
+    FOR EACH ROW 
+EXECUTE PROCEDURE update_updated_at_column();
+
+-------------------------------------------------------------------------
 -- ALTER ASSET  ---------------------------------------------------------
 ALTER TABLE asset 
     ADD COLUMN address TEXT DEFAULT '' NOT NULL;
@@ -27,3 +49,5 @@ ALTER TABLE contract
 
 ALTER TABLE asset 
     DROP COLUMN IF EXISTS address;
+
+DROP TABLE IF EXISTS identity;
